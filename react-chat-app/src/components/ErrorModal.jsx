1f4548dd0738391ab1b1fbc15613f7509fdb3fa0
goodness chris-ugari <goodnesschrisugari@yahoo.com>
2020-08-06 11:52:02 +0100

    added components

---
+++ react-chat-app/src/components/ErrorModal.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function ErrorModal() {
+    return (
+        <p>This is the error modal</p>
+    )
+}
+
+export default ErrorModal
---
+++ react-chat-app/src/components/UserList.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function UserList() {
+    return (
+        <p>This is the user list</p>
+    )
+}
+
+export default UserList
