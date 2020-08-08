070b7949a31567fc7792fde8856500b197ce02b2
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-08-08 18:52:51 +0100

    full app

--- react-chat-app/src/setupTests.js
+++ react-chat-app/src/setupTests.js
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+const Enzyme = require("enzyme");
+// this is where we reference the adapter package we installed
+// earlier
+const EnzymeAdapter = require("enzyme-adapter-react-16");
+// This sets up the adapter to be used by Enzyme
+Enzyme.configure({ adapter: new EnzymeAdapter() });
