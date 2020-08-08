8f46dd2f9259906b639953096640e249ddbe0be6
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-08-08 09:43:29 +0100

    UserList component

--- react-chat-app/src/components/UserList.js
+++ react-chat-app/src/components/UserList.js
@@ -1,9 +1,80 @@
-import React from "react"
+import React, { Component } from "react";
+import { ChatList } from "react-chat-elements";
+import FormControl from "react-bootstrap/lib/FormControl";
+import FormGroup from "react-bootstrap/lib/FormGroup";
 
-function UserList() {
+export default class UserList extends Component {
+  state = {
+    userData: [],
+    searchQuery: null,
+  };
+  componentDidMount() {}
+  searchInput(e) {
+    let value = e.target.value;
+    let searchQuery = null;
+    if (value) {
+      searchQuery = value;
+    }
+    this.setState({ searchQuery });
+  }
+
+  /* Filter logic */
+  getFilteredUserList() {
+    return !this.state.searchQuery
+      ? this.props.userData
+      : this.props.userData.filter((user) =>
+          user.name.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+        );
+  }
+
+  render() {
+    let users = this.getFilteredUserList();
     return (
-        <p>This is the user list</p>
-    )
+      <div>
+        <FormGroup>
+          <FormControl
+            type="text"
+            placeholder="Search for a user here..."
+            onInput={this.searchInput.bind(this)}
+          />
+        </FormGroup>
+        {users.length ? (
+          <ChatList
+            className={!this.props.showSignInList ? "chat-list" : "user-list"}
+            dataSource={users.map((f, i) => {
+              let date = null;
+              let subtitle = "";
+              if (
+                !this.props.showSignInList &&
+                f.messages &&
+                f.messages.length
+              ) {
+                let lastMessage = f.messages[f.messages.length - 1];
+                date = new Date(lastMessage.timeStamp);
+                subtitle =
+                  (lastMessage.position === "right" ? "You: " : f.name + ": ") +
+                  lastMessage.text;
+              }
+              return {
+                avatar: require(`../static/images/avatar/${f.id}.jpg`),
+                alt: f.name,
+                title: f.name,
+                subtitle: subtitle,
+                date: date,
+                unread: f.unread,
+                user: f,
+              };
+            })}
+            onClick={
+              !this.props.showSignInList
+                ? this.props.onChatClicked
+                : this.props.onUserClicked
+            }
+          />
+        ) : (
+          <div className="text-center no-users">No contact to show.</div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default UserList
